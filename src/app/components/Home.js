7686e9266f2abd15bf2ec665f0e50ef1f033d424
san <skysan@gmail.com>
2017-04-16 21:30:34 +0530

    added dynamically loading updating component

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,16 @@ import React from "react";
 
 export class Home extends React.Component {
     render() {
+        let content = "";
+        if (this) {
+            content = <p>Hello !</p>;
+        }
         return (
             <div>
             <p> In a new component.</p>
+
+                {content}
+                { 5 ==2 ? "Yes" : "No"}
         </div>
         );
     }
