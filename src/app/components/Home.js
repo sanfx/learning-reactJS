42c40fba3b31f454874c266a271af83621ab6188
san <skysan@gmail.com>
2017-04-17 22:00:29 +0530

    introdiced events

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,33 +1,29 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props){
+        super();
+        this.age = props.age;
+    }
+
+    onMakeOlder() {
+        this.age += 3;
+        console.log(this.age);
+    }
     render() {
-        var text = "Something";
 
         return (
             <div>
                 <p> In a new component.</p>
-                <p>  {text}</p>
-
-                <p> Your name {this.props.name}, your age is {this.props.age}</p>
-                <p> User Object: Name => {this.props.user.name} </p>
-                <div>
-                    <h4> Hobbies</h4>
-                    <ul>
-                            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-
-                </div>
+                <p> Your name {this.props.name}, your age is {this.age}</p>
                 <hr />
-                {this.props.children}
-        </div>
+                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older !</button>
+            </div>
         );
     }
 }
 
 Home.propTypes = {
     name: React.PropTypes.string,
-    age: React.PropTypes.number,
-    user: React.PropTypes.object,
-    children: React.PropTypes.element.isRequired
+    age: React.PropTypes.number
 };
