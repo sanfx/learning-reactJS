8ebb6759fedf95ef5996580b322a3ee924a17e1b
san <skysan@gmail.com>
2017-04-26 23:17:58 +0530

    implemented navigation and passing parameters

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {browserHistory} from "react-router";
 
 export class User extends React.Component{
+    onClickNavigateHome(){
+        browserHistory.push("/home")
+    }
+
     render(){
         return (
             <div>
                 <h3>The User page</h3>
-                <p>user ID:</p>
+                <p>user ID: {this.props.params.id}</p>
+                <button onClick={this.onClickNavigateHome} className="btn btn-primary">Go Home !</button>
             </div>
         );
     }
